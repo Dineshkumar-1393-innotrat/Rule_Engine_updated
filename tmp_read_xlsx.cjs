565d009267e42124e6a80872c4b784f7f040a425
Dineshkumar-1393-innotrat <dineshkumar@innotrat.in>
2026-05-18 12:53:34 +0530

    chore: untrack and ignore all root-level specifications, postman configurations, documents and scripts

--- tmp_read_xlsx.cjs
+++
@@ -1,9 +0,0 @@
-const xlsx = require('xlsx');
-try {
-    const workbook = xlsx.readFile('SRS Inputs from STLA.xlsx');
-    const sheetName = workbook.SheetNames[0];
-    console.log("SheetName: ", sheetName);
-    const sheet = workbook.Sheets[sheetName];
-    const jsonData = xlsx.utils.sheet_to_json(sheet);
-    console.log(JSON.stringify(jsonData.slice(0, 10), null, 2));
-} catch (e) { console.error(e); }
